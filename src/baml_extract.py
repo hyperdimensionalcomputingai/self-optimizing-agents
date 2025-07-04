00d97ae31840b0c17198eabd557a3ca606308b7a
prrao87 <prrao87@gmail.com>
2025-07-04 10:41:48 -0400

    Clean up eval code for extraction pipeline

--- src/baml_extract.py
+++ src/baml_extract.py
@@ -92,9 +92,14 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--start", type=int, default=1, help="Start index")
-    parser.add_argument("--end", type=int, default=8, help="End index")
+    parser.add_argument("--end", type=int, default=10, help="End index")
     parser.add_argument("--fname", type=str, default="../data/note.jsonl", help="Input file name")
-    parser.add_argument("--output", type=str, default="../data/results/extracted_fhir.jsonl", help="Output file name")
+    parser.add_argument(
+        "--output",
+        type=str,
+        default="../data/results/extracted_fhir.jsonl",
+        help="Output file name",
+    )
     args = parser.parse_args()
     if args.start < 1:
         raise ValueError("Start index must be 1 or greater")
